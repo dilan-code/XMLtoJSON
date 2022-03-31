1742574ae05a24b4cca4378ff580239504c2a870
Dilan Abed-Ali <67440662+dilan-code@users.noreply.github.com>
2022-03-31 14:19:49 +0200

    Add files via upload

---
+++ XmlToJson/XmlToJson/Consignment.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace XmlToJson
+{
+    // Consignment class, containing all data to be serialized into json.
+    class Consignment
+    {
+        [JsonProperty("consignmentId")]
+        public string ConsignmentId { get; set; }
+
+        [JsonProperty("packageIds")]
+        public List<string> PackageIds { get; set; }
+    }
+}
